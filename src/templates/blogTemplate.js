38bce3fae6b4157b9172be824e0b6b1bea670428
steve9ll <qiuliaolin1@163.com>
2019-09-20 12:04:04 +0800

    add feature: comment in blog

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,14 +1,29 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import "../pages/style/blog.scss"
 import SEO from "../components/seo"
+import "gitalk/dist/gitalk.css"
+import Gitalk from "gitalk"
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   console.log(data)
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
+  useEffect(() => {
+    var gitalk = new Gitalk({
+      clientID: "8f16fd9d681947e2f3f8",
+      clientSecret: "dfb43d6f99c32565cb14c109e03fcd42b417954b",
+      repo: "my_gatsby_blog",
+      owner: "steve9II",
+      admin: ["steve9II"],
+      id: window.location.pathname, // Ensure uniqueness and length less than 50
+      distractionFreeMode: false, // Facebook-like distraction free mode
+    })
+
+    gitalk.render("gitalk-container")
+  }, [])
   return (
     <Layout>
       <SEO
@@ -17,13 +32,13 @@ export default function Template({
       />
       <div className="blog-post-container">
         <div className="blog-post">
-          <h1>{frontmatter.title}</h1>
           <h2>{frontmatter.date}</h2>
           <div
             className="blog-post-content"
             dangerouslySetInnerHTML={{ __html: html }}
           />
         </div>
+        <div id="gitalk-container"></div>
       </div>
     </Layout>
   )
