f933c628188ee7bfecdea108884dc9845a34112e
steve9ll <qiuliaolin1@163.com>
2019-10-25 22:43:24 +0800

    add wathing data

---
+++ src/watching/mybook.js
@@ -0,0 +1,49 @@
+const mybook = [
+  {
+    img: "https://img1.doubanio.com/view/subject/m/public/s32285289.jpg",
+    src: "https://book.douban.com/subject/30320889/",
+    title: "美的进化",
+  },
+  {
+    title: "产品体验要素",
+    src: "https://book.douban.com/subject/6523997/",
+    img: "https://img1.doubanio.com/view/subject/m/public/s6519128.jpg",
+  },
+  {
+    title: "js忍者秘籍",
+    src: "https://book.douban.com/subject/30143702/",
+    img: "https://img3.doubanio.com/view/subject/m/public/s29689025.jpg",
+  },
+  {
+    title: "图解Http",
+    src: "https://book.douban.com/subject/25863515/",
+    img: "https://img3.doubanio.com/view/subject/m/public/s27283822.jpg",
+  },
+  {
+    title: "自卑与超越",
+    src: "https://book.douban.com/subject/3389960/",
+    img: "https://img1.doubanio.com/view/subject/m/public/s32285289.jpg",
+  },
+  {
+    title: "Web高效编程与优化实践",
+    src: "https://book.douban.com/subject/30170670/",
+    img: "https://img1.doubanio.com/view/subject/m/public/s29715097.jpg",
+  },
+  {
+    title: "时间简史",
+    src: "https://book.douban.com/subject/1000445/",
+    img: "https://img1.doubanio.com/view/subject/l/public/s3437737.jpg",
+  },
+  {
+    title: "齐泽克的笑话",
+    src: "https://book.douban.com/subject/26838875/",
+    img: "https://img3.doubanio.com/view/subject/m/public/s29343332.jpg",
+  },
+  {
+    title: "行为科学统计",
+    src: "https://book.douban.com/subject/3171735/",
+    img: "https://img9.doubanio.com/view/subject/m/public/s3226976.jpg",
+  },
+]
+
+export default mybook
---
+++ src/watching/mymovie.js
@@ -0,0 +1,58 @@
+const myMovies = [
+  {
+    title: "风骚律师",
+    src: "https://movie.douban.com/subject/26748847/",
+    img:
+      "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2442801582.jpg",
+  },
+  {
+    title: "绝命毒师",
+    src: "https://movie.douban.com/subject/6952149/",
+    img:
+      "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p1579021082.jpg",
+  },
+  {
+    title: "美丽人生",
+    src: "https://movie.douban.com/subject/1292063/",
+    img:
+      "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p510861873.jpg",
+  },
+  {
+    title: "搏击俱乐部",
+    src: "https://movie.douban.com/subject/1292000/",
+    img:
+      "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p1910926158.jpg",
+  },
+  {
+    title: "国王的演讲",
+    src: "https://movie.douban.com/subject/4023638/",
+    img:
+      "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p768879237.jpg",
+  },
+  {
+    title: "触不可及",
+    src: "https://movie.douban.com/subject/6786002/",
+    img:
+      "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p1454261925.jpg",
+  },
+  {
+    title: "我的大叔",
+    src: "https://movie.douban.com/subject/27602137/",
+    img:
+      "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2520568437.jpg",
+  },
+  {
+    title: "寄生虫",
+    src: "https://movie.douban.com/subject/27010768/",
+    img:
+      "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2561439800.jpg",
+  },
+  {
+    title: "你好布拉德",
+    src: "https://movie.douban.com/subject/26981174/",
+    img:
+      "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2497190395.jpg",
+  },
+]
+
+export default myMovies
