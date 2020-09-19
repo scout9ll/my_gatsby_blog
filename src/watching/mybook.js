65ff640ea03cee24910eba740784ebe60f040d97
steve9II <qiuliaolin1@163.com>
2020-09-20 00:54:57 +0800

    watch(comment)

--- src/watching/mybook.js
+++ src/watching/mybook.js
@@ -78,7 +78,7 @@ const mybooks = [
     title: "世界边缘的秘密",
     src: "https://book.douban.com/subject/34429935/",
     img: "https://img3.doubanio.com/view/subject/s/public/s33309492.jpg",
-    shortComment: "从夸父逐日到量子力学，无边无际的宇宙大到无法想象"
+    shortComment: "从夸父逐日到量子力学，大到无边无际的宇宙可能只是一种概率波，‘我’所经历的世界或许只是‘镜中影’，每个人都存在一个属于自己的世界边缘"
   },
 ]
 
